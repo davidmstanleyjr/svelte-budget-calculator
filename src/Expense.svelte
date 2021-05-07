2c3ba64e94caeecfab742ccf5c09f31f507bccef
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-05-07 16:54:21 -0400

    worked in exepense.svelte. Can now click and display amounts

--- src/Expense.svelte
+++ src/Expense.svelte
@@ -2,15 +2,23 @@
    export let id;
    export let name = '';
    export let amount = 0;
+   let displayAmount = false;
+
+   //the new value of the displayAmount is gonna be the opposite of what it currently is
+   function toggleAmount () {
+       displayAmount = !displayAmount;
+   }
 </script>
 
 <article class='single-expense'>
 
     <div class='expense-info'>
-<h2>{name} <button class='amount-btn'>
+<h2>{name} <button class='amount-btn' on:click={toggleAmount}>
     <i class='fas fa-caret-down' />
 </button></h2>
+{#if displayAmount}
 <h4>amount : ${amount}</h4>
+{/if}
     </div>
     <div class='expense-buttons'>
       <button class='expense-btn edit-btn'>
