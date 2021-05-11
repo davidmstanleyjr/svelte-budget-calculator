e292e8e7dbce7941fa37eb1bbe81264b652cec23
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-05-10 21:18:26 -0400

    added forms

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
 	import Navbar from './Navbar.svelte';
 	import ExpensesList from './ExpensesList.svelte';
 	import Totals from './Totals.svelte';
+	import ExpenseForm from './ExpenseForm.svelte';
 	//data
 	import expensesData from './expenses';
 	//variables
@@ -27,6 +28,7 @@ setContext('remove', removeExpense)
 
 <Navbar />
 <main class='content'>
+<ExpenseForm /> 
 <Totals title='total expenses' {total} />	
 	<ExpensesList expenses={expenses} />
 	<button type='button' class='btn btn-primary btn-block' on:click={clearExpenses}>clear expenses</button>
