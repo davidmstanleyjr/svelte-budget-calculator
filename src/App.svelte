be818685c7a6ba9e8b8d147497d52669f7c9c7ea
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-05-12 18:13:47 -0400

    edit expense can now be updated in the browser

--- src/App.svelte
+++ src/App.svelte
@@ -40,7 +40,12 @@
 		 setAmount = expense.amount;
 	}
 	function editExpense({ name, amount}) {
-		console.log({ name, amount })
+		expenses = expenses.map(item =>{
+			return item.id === setId? {...item,name,amount } : {...item};
+		});
+		setId= null;
+		setAmount = null;
+		setName = '';
 	}
 
 // setContext
