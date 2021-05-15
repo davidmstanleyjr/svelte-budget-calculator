eac14d7889abed5b12c06e81b0d9ff2812292c9e
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-05-15 14:18:37 -0400

    did some styling

--- src/ExpenseForm.svelte
+++ src/ExpenseForm.svelte
@@ -47,17 +47,17 @@ function handleSubmit() {
 
 <section class='form'>
     <Title title='add expense' />
-    <form class="expense-form" on:submit|preventDefault={handleSubmit}>
+    <form class='expense-form' on:submit|preventDefault={handleSubmit}>
         <div class='form-control'>
-        <label for="name">name</label>
-        <input type="text" id='name' bind:value={name} />
+        <label for='name'>name</label>
+        <input type='text' id='name' bind:value={name} />
         </div>
         <div class='form-control'>
-            <label for="amount">amount</label>
-            <input type="number" id='amount' bind:value={amount} />
+            <label for='amount'>amount</label>
+            <input type='number' id='amount' bind:value={amount} />
             </div>
             {#if isEmpty}
-            <p class="form-empty">
+            <p class='form-empty'>
                 please fill out all form fields
             </p>
             {/if}
@@ -65,7 +65,7 @@ function handleSubmit() {
         {#if isEditing}edit expense{:else}add expense {/if}
     </button>
     <button type='button' class='close-btn' on:click={hideForm}>
-    <i class="fas fa-times" />
+    <i class='fas fa-times' />
         close
     </button>
     </form>
